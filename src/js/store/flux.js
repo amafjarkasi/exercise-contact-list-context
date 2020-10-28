15083e3fe3c2054ec3a4772d172bfda918ccabe4
amafjarkasi <amafjarkasi@gmail.com>
2020-10-28 23:00:30 +0000

    latest updates

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,27 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+            addContact : () => {
+
+
+            },
+            updateContact : () => {
+
+
+            },
+            deleteContact : () => {
+
+
+            },
+            getContact : () => {
+
+
+            }
+    
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
