98f3e21d2db497dbc5bd3e3dc9655b3b1c24c0ba
amafjarkasi <amafjarkasi@gmail.com>
2020-10-28 23:13:01 +0000

    latest updates

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,30 +1,67 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+            contacts : []
 
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
-            addContact : () => {
+            addContact : (full_name, email, phone, address) => {
 
 
             },
-            updateContact : () => {
+            updateContact : (id, full_name, email, phone, address) => {
 
 
             },
-            deleteContact : () => {
+            deleteContact : (id) => {
 
 
             },
-            getContact : () => {
-
-
+            getContacts : () => {
+                 contacts = [
+                     {
+                        "id": "1472",
+                        "agenda_slug": "amafjarkasi",
+                        "full_name": "Dave Bradley",
+                        "email": "dave@gmail.com",
+                        "phone": "7864445566",
+                        "address": "47568 NW 34ST, 33434 FL, USA",
+                        "created_at": "2020-10-26 23:18:58"
+                    },
+                     {
+                        "id": "1473",
+                        "agenda_slug": "amafjarkasi",
+                        "full_name": "Dave Bradley",
+                        "email": "dave@gmail.com",
+                        "phone": "7864445566",
+                        "address": "47568 NW 34ST, 33434 FL, USA",
+                        "created_at": "2020-10-26 23:18:58"
+                    },
+                     {
+                        "id": "1474",
+                        "agenda_slug": "amafjarkasi",
+                        "full_name": "Dave Bradley",
+                        "email": "dave@gmail.com",
+                        "phone": "7864445566",
+                        "address": "47568 NW 34ST, 33434 FL, USA",
+                        "created_at": "2020-10-26 23:18:58"
+                    },
+                     {
+                        "id": "1475",
+                        "agenda_slug": "amafjarkasi",
+                        "full_name": "Dave Bradley",
+                        "email": "dave@gmail.com",
+                        "phone": "7864445566",
+                        "address": "47568 NW 34ST, 33434 FL, USA",
+                        "created_at": "2020-10-26 23:18:58"
+                    }
+                ];
+                setStore(contacts);
             }
-    
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
-		}
+        }
 	};
 };
 
